6fe5d7d56a696f092f10c39869865084b668edc9
unknown <elmurodvkh@gmail.com>
2025-02-18 16:11:42 +0500

    start papka o'chirib tashlandi, undagi kodlar index.js -ga o'tkazildi

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
 const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 const app = express();
 require("dotenv").config();
-require("./start/routes")(app);
-require("./start/db")(app);
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// static files route
+app.use("/uploads", express.static("uploads"));
+
+// routes
+app.get("/", (_, res) => res.send("Hello world!"));
+app.use("/api/products", require("./routes/products.routes"));
+app.use("/api/videos", require("./routes/videos.routes"));
+app.use("/api/upload", require("./routes/uploads.routes"));
+app.use("/api/email", require("./routes/email.routes"));
+
+// mongodb connection
+const PORT = process.env.PORT || 5000;
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => {
+        console.log("mongodb successfully connected!");
+        app.listen(PORT, () => console.log(`server running on port ${PORT}`));
+    })
+    .catch(error => {
+        console.log("mongodb connection error: ", error.message);
+        process.exit(1);
+    })
